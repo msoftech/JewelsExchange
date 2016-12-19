45deb2afe77630cfc033edbac3aa200d9ec0cbac
Mohamed Abubakkar Shiddic <kannan@msoftech.com>
2016-12-19 17:33:00 +0530

    q

--- iOS/ViewController_StockDetails.designer.cs
+++ iOS/ViewController_StockDetails.designer.cs
@@ -16,35 +16,85 @@ namespace JewelsExchange.iOS
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton btnHome { get; set; }
+        UIKit.UILabel lblBaseDesc { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UILabel lblDetails { get; set; }
+        UIKit.UILabel lblCategory { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UILabel lblDetails2 { get; set; }
+        UIKit.UILabel lblCloseoutPrice { get; set; }
 
-        [Action ("BtnHome_TouchUpInside:")]
+        [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        partial void BtnHome_TouchUpInside (UIKit.UIButton sender);
+        UIKit.UILabel lblCurrency { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblKarat { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblRegion { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblSellingPrice { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblStock { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblWeight { get; set; }
 
         void ReleaseDesignerOutlets ()
         {
-            if (btnHome != null) {
-                btnHome.Dispose ();
-                btnHome = null;
+            if (lblBaseDesc != null) {
+                lblBaseDesc.Dispose ();
+                lblBaseDesc = null;
             }
 
-            if (lblDetails != null) {
-                lblDetails.Dispose ();
-                lblDetails = null;
+            if (lblCategory != null) {
+                lblCategory.Dispose ();
+                lblCategory = null;
             }
 
-            if (lblDetails2 != null) {
-                lblDetails2.Dispose ();
-                lblDetails2 = null;
+            if (lblCloseoutPrice != null) {
+                lblCloseoutPrice.Dispose ();
+                lblCloseoutPrice = null;
+            }
+
+            if (lblCurrency != null) {
+                lblCurrency.Dispose ();
+                lblCurrency = null;
+            }
+
+            if (lblKarat != null) {
+                lblKarat.Dispose ();
+                lblKarat = null;
+            }
+
+            if (lblRegion != null) {
+                lblRegion.Dispose ();
+                lblRegion = null;
+            }
+
+            if (lblSellingPrice != null) {
+                lblSellingPrice.Dispose ();
+                lblSellingPrice = null;
+            }
+
+            if (lblStock != null) {
+                lblStock.Dispose ();
+                lblStock = null;
+            }
+
+            if (lblWeight != null) {
+                lblWeight.Dispose ();
+                lblWeight = null;
             }
         }
     }
